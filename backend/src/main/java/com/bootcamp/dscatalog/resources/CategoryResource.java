57a4012c31ab9766c71083a510ffc8e6668c1cc3
GiancarloJr <giancarlo456@hotmail.com>
2022-09-14 23:16:07 -0300

    H2 database

--- backend/src/main/java/com/bootcamp/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/bootcamp/dscatalog/resources/CategoryResource.java
@@ -2,26 +2,33 @@ package com.bootcamp.dscatalog.resources;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bootcamp.dscatalog.entities.Category;
+import com.bootcamp.dscatalog.services.CategoryServices;
 
 @RestController
 @RequestMapping("/category")
 public class CategoryResource {
-	
+
+	@Autowired
+	private CategoryServices categoryServices;
+
 	@GetMapping("/")
-	public ResponseEntity<List<Category>> findAll(){
-		List<Category> list = new ArrayList<>();
-		list.add(new Category(1L, "Books"));
-		list.add(new Category(2L, "Eletronics"));
-		
-		return ResponseEntity.ok().body(list);
+	public ResponseEntity<List<Category>> findAll() {
+		return ResponseEntity.ok().body(categoryServices.findAll());
 	}
-	
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Optional<Category>> findById(@PathVariable Long id) {
+		return ResponseEntity.ok().body(categoryServices.findById(id));
+	}
 
 }

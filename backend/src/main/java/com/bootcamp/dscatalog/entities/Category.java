56c21ed9a20dac14e0b9f79e4c207ac419e0bd16
GiancarloJr <giancarlo456@hotmail.com>
2022-09-28 01:00:13 -0300

    Entidade Product

--- backend/src/main/java/com/bootcamp/dscatalog/entities/Category.java
+++ backend/src/main/java/com/bootcamp/dscatalog/entities/Category.java
@@ -20,6 +20,7 @@ public class Category{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
 	private String name;	
 
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")

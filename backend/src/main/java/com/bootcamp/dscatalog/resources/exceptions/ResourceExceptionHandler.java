e5f07d20b5e317b3ade699c44035fb72433d8f79
GiancarloJr <giancarlo456@hotmail.com>
2022-10-19 22:56:51 -0300

    Tratamento de Erro de Validação

--- backend/src/main/java/com/bootcamp/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/bootcamp/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,7 @@ import com.bootcamp.dscatalog.services.exceptions.DataBaseException;
 import com.bootcamp.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -30,6 +31,17 @@ public class ResourceExceptionHandler {
             error.setTimestamp(Instant.now());
             error.setStatus(HttpStatus.BAD_REQUEST.value());
             error.setError("Database Exception");
+            error.setMessage(e.getMessage());
+            error.setPath(request.getRequestURI());
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public ResponseEntity<StandardError> Validation(MethodArgumentNotValidException e, HttpServletRequest request){
+            StandardError error = new StandardError();
+            error.setTimestamp(Instant.now());
+            error.setStatus(HttpStatus.UNPROCESSABLE_ENTITY.value());
+            error.setError("Argument Does Not Valid");
             error.setMessage(e.getMessage());
             error.setPath(request.getRequestURI());
 
